6ac20b9e719c9b798f458d52c1fcb597722873c6
Maurice Rabb <maurice@devbootcamp.com>
2015-09-06 13:21:11 -0500

    Finish model

--- js/game.js
+++ js/game.js
@@ -1,15 +1,19 @@
 // https://javascriptobfuscator.com/Javascript-Obfuscator.aspx
 
+// jshint globalstrict:true
+/* globals Util: false */
 
-var POSITIONS = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-var VALUES = [0, 2, 4, 8, 16, 32, 64, 128, 256];
+"use strict";
+
+var POSITIONS   = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+var VALUES      = [0, 2, 4, 8, 16, 32, 64, 128, 256];
 var SEED_VALUES = [2, 2, 2, 4];
 
 var LEFT_TO_RIGHT = "L";
 var RIGHT_TO_LEFT = "R";
 var TOP_TO_BOTTOM = "T";
 var BOTTOM_TO_TOP = "B";
-var DIRECTIONS = "L R T B".split(" ");
+var SIDES         = "Left Right Top Bottom".split(" ");
 
 
 function TwoFiftySixGame(valuesString_) {
@@ -34,21 +38,23 @@ TwoFiftySixGame.prototype.copy = function () {
 
 TwoFiftySixGame.prototype.verifyAndSet = function (valuesString) {
   // var cells = cellsString.match(/\D+/);
-  var words, values, index, zeroes;
+  var words, values, index, zeroes, value;
+
+  function asNumber(word) { return parseInt(word); }
 
   words = valuesString.match(/\d+/g);
-  if (words.length !== SIZE) {
+  if (words.length !== 16) {
     throw new Error("String must contain 16 numbers!");
   }
 
-  values = words.map(parseInt);
+  values = words.map(asNumber);
   index = 0;
   zeroes = 0;
 
   while (index < 16) {
     value = values[index++];
     if (value) {
-      if (!isPowerOfTwo(value)) {
+      if (!Util.isPowerOfTwo(value)) {
         throw new Error("Non-zero numbers must be powers of 2!");
       }
     } else {
@@ -64,7 +70,7 @@ TwoFiftySixGame.prototype.verifyAndSet = function (valuesString) {
 
 TwoFiftySixGame.prototype.initializeValues = function () {
   this.values = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
-  this.addNewValues();
+  this.addNewValues(2);
 };
 
 TwoFiftySixGame.prototype.emptyPositions = function () {
@@ -75,38 +81,45 @@ TwoFiftySixGame.prototype.emptyPositions = function () {
   return positions;
 };
 
-TwoFiftySixGame.prototype.addNewValues = function () {
-  var emptyPositions = this.emptyPositions();
-  var newPositions = samples(emptyPositions, 2);
-  var newValues = sample(SEED_VALUES, 2);
-
-  this.values[newPositions[0]] = newValue[0];
-  this.values[newPositions[1]] = newValue[1];
-  return this;
+TwoFiftySixGame.prototype.filledPositions = function () {
+  var positions = [];
+  this.values.forEach(function (value, index) {
+    if (value > 0) { positions.push(index); }
+  });
+  return positions;
 };
 
+TwoFiftySixGame.prototype.addNewValues = function (count) {
+  var emptyPositions = this.emptyPositions();
+  var newPositions = Util.samples(emptyPositions, count);
+  var newValues = Util.samples(SEED_VALUES, count);
+  var index = 0;
+  do {
+    this.values[newPositions[index]] = newValues[index];
+  } while (++index < count);
+  return this;
+};
 
 TwoFiftySixGame.prototype.toString = function () {
   var word, words, sliceIndex;
   words = [];
   sliceIndex = 0;
   while (sliceIndex < 4) {
-    word = this.getSlice(sliceIndex).join(" ");
+    word = this.getSlice(sliceIndex++).join(" ");
     words.push(word);
   }
   return words.join("   ");
 };
 
-TwoFiftySixGame.prototype.forSlice = function (rowColomIndex, direction, action) {
+TwoFiftySixGame.prototype.forSlice = function (sliceIndex, direction, action) {
   var values = this.values;
-  var direction = direction_ || "LEFT_TO_RIGHT";
-  var slice, position, increment, count, value;
+  var position, increment, count, value;
 
   switch (direction[0].toUpperCase()) {
-    case LEFT_TO_RIGHT : position =  0; increment =  1; break;
-    case RIGHT_TO_LEFT : position =  3; increment = -1; break;
-    case TOP_TO_BOTTOM : position =  0; increment =  4; break;
-    case BOTTOM_TO_TOP : position = 12; increment = -4; break;
+    case LEFT_TO_RIGHT : position =  sliceIndex * 4    ; increment =  1; break;
+    case RIGHT_TO_LEFT : position =  sliceIndex * 4 + 3; increment = -1; break;
+    case TOP_TO_BOTTOM : position =  sliceIndex        ; increment =  4; break;
+    case BOTTOM_TO_TOP : position =  sliceIndex + 12   ; increment = -4; break;
   }
   count = 4;
   while (count--) {
@@ -114,90 +127,89 @@ TwoFiftySixGame.prototype.forSlice = function (rowColomIndex, direction, action)
     action.call(this, value, position);
     position += increment;
   }
-}:
+  return this;
+};
 
 
-TwoFiftySixGame.prototype.getSlice = function (rowColomIndex, direction_) {
+TwoFiftySixGame.prototype.getSlice = function (sliceIndex, direction_) {
   var values = this.values;
   var direction = direction_ || "LEFT_TO_RIGHT";
   var slice, index, increment, count, value;
 
   slice = [];
-  switch (direction[0].toUpperCase()) {
-    case LEFT_TO_RIGHT : index =  0; increment =  1; break;
-    case RIGHT_TO_LEFT : index =  3; increment = -1; break;
-    case TOP_TO_BOTTOM : index =  0; increment =  4; break;
-    case BOTTOM_TO_TOP : index = 12; increment = -4; break;
-  }
-  count = 4;
-  while (count--) {
-    value = values[index];
-    index += increment;
+  this.forSlice(sliceIndex, direction, function (value) {
     slice.push(value);
-  }
+  });
   return slice;
 };
 
-TwoFiftySixGame.prototype.putSlice = function (rowColomIndex, direction, slice) {
+TwoFiftySixGame.prototype.putSlice = function (sliceIndex, direction, slice) {
   var values = this.values;
-  var index, increment;
+  var index = 0;
 
-  switch (direction[0].toUpperCase()) {
-    case LEFT_TO_RIGHT : index =  0; increment =  1; break;
-    case RIGHT_TO_LEFT : index =  3; increment = -1; break;
-    case TOP_TO_BOTTOM : index =  0; increment =  4; break;
-    case BOTTOM_TO_TOP : index = 12; increment = -4; break;
-  }
-  slice.forEach(function (value) {
-    values[index] = value;
-    index += increment;
+  this.forSlice(sliceIndex, direction, function (_value, position) {
+    values[position] = slice[index++];
   });
   return this;
 };
 
+
 TwoFiftySixGame.prototype.smashValues = function (slice) {
-  var startValues = slice.slice();
-  var smashValues = [0, 0, 0, 0];
-  var currentValue, nextValue;
+  var sourceValues = slice.filter(Util.isGreaterThanZero);
+  var smashedValues = [0, 0, 0, 0];
+  var currentValue, nextValue, fromIndex, toIndex;
 
-  while (true) {
-    do {
-      currentValue = startValues.unshift();
-      if (currentValue === undefined) { return smashValues; }
-    } while (currentValue === 0);
+  fromIndex = toIndex = 0;
 
-    while (true) {
-      nextValue = startValues.unshift();
-      if (nextValue === undefined) { return smashValues; }
-      if (currentValue === nextValue) {
-        this.score += (smashValues[nextIndex++] = currentValue + nextValue);
-        break;
-      }
-      smashValues[nextIndex++] = currentValue;
+  currentValue = sourceValues[fromIndex++];
+
+  while (currentValue) {
+    nextValue = sourceValues[fromIndex++];
+
+    if (!nextValue) {
+      smashedValues[toIndex] = currentValue;
+      break;
+    }
+
+    if (currentValue === nextValue) {
+      this.score += (smashedValues[toIndex++] = currentValue + nextValue);
+      currentValue = sourceValues[fromIndex++];
+    } else {
+      smashedValues[toIndex++] = currentValue;
       currentValue = nextValue;
     }
   }
+  return smashedValues;
 };
 
-TwoFiftySixGame.prototype.smash = function (direction) {
-  var index, values, smashedValues;
+
+TwoFiftySixGame.prototype.smash = function (toSide) {
+  var index, isChanged, direction, values, smashedValues;
   index = 4;
+  isChanged = false;
+  direction = toSide;
+
   while (index--) {
     values = this.getSlice(index, direction);
     smashedValues = this.smashValues(values);
-    this.putSlice(index, direction, smashValues);
+    if (!Util.areEqualArrays(values, smashedValues)) {
+      this.putSlice(index, direction, smashedValues);
+      isChanged = true;
+    }
   }
-  return this.addNewValues();
+  return isChanged;
+};
+
+TwoFiftySixGame.prototype.move = function (toSide) {
+  return this.smash(toSide) ? this.addNewValues(1) : this;
 };
 
 TwoFiftySixGame.prototype.isDone = function () {
-  var isFilled = this.values.every(function (value) { return value > 0; });
-  if (!isFilled) { return false; }
+  var index = SIDES.length;
 
-  index = DIRECTIONS.length;
-  while (index--) {
-    direction = DIRECTIONS[index];
-    if (this.copy().smash(direction).score !== this.score) { return false; }
+  if (!this.values.every(Util.isGreaterThanZero)) { return false; }
+	while (index--) {
+    if (this.copy().smash(SIDES[index])) { return false; }
   }
   return true;
-}
+};
--- js/util.js
+++ js/util.js
@@ -1,8 +1,17 @@
-Util = {};
+"use strict";
+
+var Util = {};
 
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/randomhttps://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Returns a random integer between min (included) and max (excluded)
 // Using Math.round() will give you a non-uniform distribution!
+
+Util.isGreaterThanZero = function (value) { return value > 0; }
+
+Util.isBetween = function (number, min, max) {
+  return min <= number && number <= max;
+};
+
 Util.getRandom = function (limit_min, limit_) {
   var min, limit;
   if (arguments.length > 1) {
@@ -13,33 +22,37 @@ Util.getRandom = function (limit_min, limit_) {
     limit = limit_min;
   }
   return Math.floor(Math.random() * (limit - min)) + min;
-}
-
-Util.isBetween = function (number, min, max) {
-  return min <= number && number <= max;
-}
+};
 
 Util.isPowerOfTwo = function (number) {
   var match = 2;
   while (match < number) { match *= 2; }
   return match === number;
-}
+};
 
 Util.sample = function (array) {
-  var index = getRandom(0, array.length);
+  var index = this.getRandom(0, array.length);
   return array[index];
-}
+};
 
 Util.samples = function (array, count) {
-  var source, destination, count, index, element;
+  var source, destination, index, element;
   source = array.slice();
   destination = [];
-  count = (arguments.length > 1 ? count : 1);
 
-  while (count) {
-    index = getRandom(0, source.length);
+  while (count--) {
+    index = this.getRandom(0, source.length);
     element = source.splice(index , 1)[0];
     destination.push(element);
   }
   return destination;
-}
+};
+
+Util.areEqualArrays = function (arrayA, arrayB) {
+  var index = arrayA.length;
+  if (index !== arrayB.length) { return false; }
+  while (index--) {
+    if (arrayA[index] !== arrayB[index]) { return false; }
+  }
+  return true;
+};
