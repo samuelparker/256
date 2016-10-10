240e56ff8cf05353115f91bb4ab9fdc75cf8190f
Chris Johnson & Sam Parker <pair+koala-t.samuelparker@devbootcamp.com>
2016-10-10 16:03:16 -0700

    add move left and right functions

--- js/game.js
+++ js/game.js
@@ -34,7 +34,77 @@ Game.prototype.toString = function(){
   return boardString;
 };
 
-game = new Game('0000200000000020');
-otherGame = new Game();
-console.log(game.board);
-console.log(game.toString());
+
+
+Game.prototype.move = function(direction){
+  switch (direction) {
+    case 'right':
+      moveRight(this.board);
+      break;
+    case 'left':
+      moveLeft(this.board);
+      break;
+  }
+}
+
+// returns a new merged and moved row
+function eachRowNumberFromLeft(row){
+  numbers = [];
+  for(var i = 0; i < row.length; i++){
+    if (row[i] > 0){
+      numbers.push(row[i]);
+    };
+  };
+  return mergeNumbers(numbers);
+};
+
+// merges the input set of numbers and returns them in an array filled with 0's
+function mergeNumbers(numbers){
+  var newRow = []
+  // iterate through the numbers
+  for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    // if it is not the last number
+    if(numbers[i+1] != undefined){
+      var nextNumber = numbers[i+1];
+      // if the next number matches the current number
+      if(number == nextNumber){
+        // merge them
+        newRow.push(number+nextNumber);
+        // skip the merged next-number
+        i++;
+      } else {
+        // if no match, add the number to the new row
+        newRow.push(numbers[i]);
+      };
+    } else {
+      // if last number, add it to the new row
+      newRow.push(numbers[i]);
+    };
+  };
+  // fill the rest of the row with zeros
+  while(newRow.length < 4){
+    newRow.push(0);
+  };
+  return newRow;
+};
+
+// returns the [number, it's index in a row] for each number in the row
+  // going right to left
+function eachRowNumberFromRight(row){
+  return eachRowNumberFromLeft(row).reverse()
+};
+
+function moveRight(board){
+  for(var i = 0; i < board.length; i++){
+    board[i] = eachRowNumberFromRight(board[i]);
+  };
+};
+
+function moveLeft(board){
+  for(var i = 0; i < board.length; i++){
+    board[i] = eachRowNumberFromLeft(board[i]);
+  };
+};
+
+
