4220a3a9f758b8e7068ace531a0dd7bd1019d80c
Chris Johnson & Sam Parker <pair+koala-t.samuelparker@devbootcamp.com>
2016-10-10 17:32:03 -0700

    display game board dynamically

--- js/application.js
+++ js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+  generateBoardView(newGame);
 });
+
+newGame = new Game;
+
+function generateBoardView(game){
+  game.board.forEach(function(row){
+    $(".game-board").append("<div class='game-row'></div>");
+    row.forEach(function(number){
+      $(".game-board").last().append("<div class='game-block'><h1>"+number+"</h1></div>");
+    });
+  });
+};
